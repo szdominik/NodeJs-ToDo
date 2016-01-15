e9150e8c4cdebd14743ac229e15f0b7c1c65d36b
Dominik Szabó <dominikblasir@gmail.com>
2016-01-15 19:09:03 +0100

    Port fix to Heroku.

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/donetodo', function(req, res) {
 	res.status(201).send({ new_todo: getToDo(id) });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 

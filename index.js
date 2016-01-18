aabd9b501d07443bbd1092b807a4370fc5bde86a
Dominik Szabó <dominikblasir@gmail.com>
2016-01-18 15:12:17 +0100

    feat: register and sign in a user, check with cookies, basic processes with users

--- index.js
+++ index.js
@@ -7,34 +7,64 @@ var request = require('superagent');
 var path = require('path');
 var moment = require('moment');
 
-var suli = {
-	id: 1,
-	name: "suli",
-	style: "primary"
-};
-var bead = {
-	id: 2,
-	name: "beadandó",
-	style: "warning"
-}
+var owners = [
+	{
+		id: 1,
+		name: "dominik"
+	},
+	{
+		id: 2,
+		name: "admin"
+	}
+]
 
-var alma = {
-	id: 1,
-	title: "Beadandót csinálni",
-	deadline: '2016.01.20.',
-	labels: [suli, bead],
-	done: true
-};
-var korte = {
-	id: 2,
-	title: 'CodeCheckert csinálni',
-	deadline: '2016.01.15.',
-	labels: [suli],
-	done: false
-};
+var labels = [
+	{
+		ownerId: 2,
+		id: 1,
+		name: "egyéb",
+		style: "info"
+	},
+	{
+		ownerId: 1,
+		id: 2,
+		name: "suli",
+		style: "primary"
+	},
+	{
+		ownerId: 1,
+		id: 3,
+		name: "beadandó",
+		style: "warning"
+	}
+]
 
-var todos = [alma, korte];
-var labels = [suli, bead];
+var todos = [
+	{
+		ownerId: 1,
+		id: 1,
+		title: 'Beadandót csinálni',
+		deadline: '2016.01.20.',
+		labels: [2, 3],
+		done: true
+	},
+	{
+		ownerId: 1,
+		id: 2,
+		title: 'CodeCheckert csinálni',
+		deadline: '2016.01.15.',
+		labels: [2],
+		done: false
+	},
+	{
+		ownerId: 2,
+		id: 3,
+		title: 'főzni',
+		deadline: '2016.02.10. 12:00',
+		labels: [1],
+		done: false
+	}
+]
 
 function sortTodos(a, b) {
 	if(a.deadline > b.deadline)
@@ -47,6 +77,12 @@ function sortTodos(a, b) {
 function getToDo(id) {
 	return todos.filter(function(item) {
 		return item.id == id;
+	})[0];
+};
+
+function getOwnerByName(name) {
+	return owners.filter(function(item) {
+		return item.name == name;
 	})[0];
 };
 
@@ -65,7 +101,7 @@ function getLabelByName(name) {
 function hasLabel(item, id) {
 	var volt = false;
 	item.labels.forEach(function(label) {
-		if(label.id == id)
+		if(label == id)
 			volt = true;
 	});
 	return volt;
@@ -85,11 +121,49 @@ app.get('/todos', function(req, res) {
 		filtered = filtered.filter(function(item) {
 			return hasLabel(item, parseInt(req.query.labelId, 10));
 		});
-	res.status(200).send({todos: filtered});
+	if(req.query.ownerId != undefined) {
+		filtered = filtered.filter(function(item) {
+			return item.ownerId == parseInt(req.query.ownerId, 10);
+		});
+		res.status(200).send({todos: filtered});
+	}
+	else
+		res.redirect('/');
 });
 
 app.get('/labels', function(req, res) {
-	res.status(200).send({labels: labels});
+	if(req.query.ownerId != undefined) {
+		var filtered = labels.filter(function(item) {
+			return item.ownerId == parseInt(req.query.ownerId, 10);
+		});
+		res.status(200).send({labels: filtered});
+	}
+	else
+		res.redirect('/');
+});
+
+app.get('/users', function(req, res) {
+	var filtered = owners.filter(function(item) {
+		return item.name == req.query.name;
+	});
+	if(filtered.length != 0)
+		res.status(200).send({ownerId: filtered[0].id});
+	else
+		res.status(404).send({name: req.query.name});
+});
+
+app.post('/users', function(req, res) {
+	if(req.body.name != '' && getOwnerByName(req.body.name) == undefined) {
+		var new_owner = {
+			id: owners.length + 1,
+			name: req.body.name
+		};
+		owners.push(new_owner);
+		res.status(200).send({ownerId: new_owner.id});
+	}
+	else {
+		res.status(404).send({name: req.body.name});
+	}
 });
 
 app.post('/todos', function(req, res) {
@@ -102,6 +176,7 @@ app.post('/todos', function(req, res) {
 		var deadline = (req.body.deadline != '') ? req.body.deadline : moment(new Date()).add(14, 'days').format('YYYY.MM.DD.');
 		
 		var new_todo = {
+			ownerId: req.body.ownerId,
 			id: todos.length + 1,
 			title: req.body.title,
 			deadline: deadline,
@@ -116,6 +191,7 @@ app.post('/todos', function(req, res) {
 app.post('/labels', function(req, res) {
 	if(getLabelByName(req.body.name) == undefined && req.body.name != '') {
 		var new_label = {
+			ownerId: req.body.ownerId,
 			id: labels.length + 1,
 			name: req.body.name,
 			style: req.body.style
